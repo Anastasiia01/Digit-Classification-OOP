Regular NN withput Batch Norm works properly with both Softmax, Sigmoid on the output. Minor errors were fixed.

NetworkOOP/Layer.py
@@ -1,4 +1,5 @@
 from ActivationType import ActivationType
+from BatchNormMode import BatchNormMode
 import numpy as np
 
 
@@ -9,13 +10,12 @@ class Layer(object):
         self.prevNumNeurons=prevNumNeurons
         self.activationFunction=activationFunction
         self.dropOut=dropOut
-        self.isLastLayer=isLastLayer       
-
+        self.isLastLayer=isLastLayer
 
         #layer parameters
         self.W=np.random.uniform(-0.1,0.1,(self.numNeurons,self.prevNumNeurons))    
         self.B=np.random.uniform(-1,1,(self.numNeurons))
-        self.sum=0
+        self.sum=None
         self.delta=np.zeros(self.numNeurons)
         self.a=None #since number of samples in dataset needed as one of the dimensions
 
@@ -24,10 +24,42 @@ class Layer(object):
         self.gradB=np.zeros(self.B.shape) #same as delta
         self.gradAF=None;#gradient of activation function
         
+        #Batch Normalization parameters
+        #s(sum) -> sHat -> sb -> activation func
+        self.mu=np.zeros(self.numNeurons)#mu is 1 x numNeurons
+        self.muRunning=np.zeros(self.numNeurons)
+        self.sigma2=np.zeros(self.numNeurons)
+        self.sigma2Running=np.zeros(self.numNeurons)
+        self.epsilon=1E-6
+        self.sHat=None#sHat is (numSamples x numNeurons), where S is centered around the mean, scaled by varience
+        
+        self.sb=None#sb is (numSamples x numNeurons), where sHat is scaled by gamma and shifted by beta
+        self.gamma=np.random.uniform(-0.1,0.1,(self.numNeurons))
+        self.beta=np.random.uniform(-1,1,(self.numNeurons))
+        self.gradGamma=np.zeros(self.numNeurons)
+        self.gradBeta=np.zeros(self.numNeurons)
+        self.deltaBn=np.zeros(self.numNeurons)#deltabn is (numSamples x numNeurons)  #grad of S
+
     
-    def Evaluate(self,input):
+    def Evaluate(self,input, batchNorm=False, batchMode=BatchNormMode.TRAIN):
         #forward prop
-        self.sum=input@self.W.T+self.B #sum is (numSamples x numNeurons)        
+        S=input@self.W.T+self.B #S is (numSamples x numNeurons)  
+        if(batchNorm==True):
+            #s(sum) -> sHat -> sb -> activation func
+            if(batchMode==BatchNormMode.TRAIN):
+                self.mu=np.mean(S,axis=0)
+                self.sigma2=np.var(S,axis=0)
+                self.muRunning=0.9*self.muRunning+(1-0.9)*self.mu
+                self.sigma2Running=0.9*self.sigma2Running+(1-0.9)*self.sigma2
+            else:   
+                self.mu=self.muRunning
+                self.sigma2=self.sigma2Running
+            self.sHat=(S-self.mu)/((self.sigma2+self.epsilon)**0.5)
+            self.sb=self.sHat*self.gamma+self.beta
+            self.sum=self.sb
+        else:#without Batch Normalization:
+            self.sum=S
+
         if(self.activationFunction==ActivationType.SIGMOID):
             self.a=self.sigmoid(self.sum)
             self.gradAF=self.a*(1-self.a)
@@ -36,8 +68,12 @@ class Layer(object):
             self.gradAF=(1-self.a*self.a)
         elif(self.activationFunction==ActivationType.RELU):
             self.a=self.Relu(self.sum)
-            self.gradAF=1.0*(self.a>0)
-        else: #self.activationFunction==ActivationType.SOFTMAX
+            #self.derivAF = 1.0 * (self.a > 0)
+            epsilon=1.0e-6
+            self.gradAF = 1. * (self.a > epsilon)
+            self.gradAF[self.gradAF == 0] = epsilon
+            #self.gradAF=1.0*(self.a>0)
+        elif(self.activationFunction==ActivationType.SOFTMAX):
             self.a=self.Softmax(self.sum)
             self.gradAF=None #we will use delta directly for back prop
         #apply dropOut unless last layer
@@ -46,21 +82,35 @@ class Layer(object):
             self.a=self.mask*self.a
             self.gradAF=self.mask*self.gradAF
         
-    def clearGradWB(self):
+    def clearGrads(self):
         self.gradW=np.zeros(self.W.shape)
         self.gradB=np.zeros(self.B.shape)
 
     def sigmoid(self,x):
         return 1/(1+np.exp(-x))
 
-    def Tanh(x):
+    def Tanh(self,x):
         return np.tanh(x)
 
-    def Relu(x):
-        return np.maximum(x,0)
+    def Relu(self,x):
+        der=np.copy(x)
+        der[x<=0]=0
+        return der
+        #return np.maximum(x,0)
 
-    def Softmax(x):
-        return np.exp(x)/np.sum(np.exp(x))
+    def Softmax(self,x):
+        ex = np.exp(x)
+        #print(ex.shape)
+        if (x.shape[0] == x.size):
+            ex = np.exp(x)
+            return ex/ex.sum()
+        for i in range(ex.shape[0]):
+            denom = ex[i].sum()
+            #print("ex ",ex[i])
+            ex[i] = ex[i]/denom
+        return ex
+
+        #return np.exp(x)/np.sum(np.exp(x))
 
 
     

NetworkOOP/NN.py
@@ -3,6 +3,7 @@ from GradientType import GradientType
 from Layer import Layer
 import numpy as np
 from sklearn.utils import shuffle
+from BatchNormMode import BatchNormMode
 
 class NN(object):
     def __init__(self, X,Y,listNeuronsInLayers, activationFunction, lastActivationFunction, dropOut=1.0):
@@ -18,16 +19,25 @@ class NN(object):
             self.listOfLayers.append(layer)
         lastLayer=Layer(listNeuronsInLayers[len(listNeuronsInLayers)-1], listNeuronsInLayers[len(listNeuronsInLayers)-2], lastActivationFunction,dropOut,True)
         self.listOfLayers.append(lastLayer)
-        #self.Evaluate()
 
-    def Evaluate(self,input): #returns output of network
-        self.listOfLayers[0].Evaluate(input)
-        for i in range(1,len(self.listOfLayers)):
-            self.listOfLayers[i].Evaluate(self.listOfLayers[i-1].a)
+    def Evaluate(self,input,batchNorm=False, batchMode=BatchNormMode.TEST): #returns output of network
+        self.listOfLayers[0].Evaluate(input,batchNorm,batchMode)
+        for i in range(1,(len(self.listOfLayers)-1)):
+            self.listOfLayers[i].Evaluate(self.listOfLayers[i-1].a,batchNorm,batchMode)
+        #do NOT apply batch Norm on output layer, especially if it is Softmax,
+        #since norm reduces the difference between outputs, what softmax is relying on while making desion 
+        self.listOfLayers[len(self.listOfLayers)-1].Evaluate(self.listOfLayers[len(self.listOfLayers)-2].a,batchNorm=False)
         return self.listOfLayers[len(self.listOfLayers)-1].a
-
-    def Train(self, gradientType, epochsNum=100, alpha=0.01, regLambda=0.01, batchNorm=False, batchSize=50):
+    
+    def Train(self, gradientType, epochsNum=50, alpha=0.1, regLambda=0.01 , batchNorm=False, Size=40):
         samplesNum=self.X.shape[0]
+        epsilon=1E-6
+        if(gradientType==GradientType.STOCHASTIC):
+            batchSize=1
+        elif(gradientType==GradientType.MINIBATCH):
+            batchSize=Size
+        else:#if(gradientType==GradientType.BATCH):
+            batchSize=samplesNum
         for j in range(epochsNum):
             self.X,self.Y=shuffle(self.X,self.Y)
             loss=0
@@ -35,9 +45,10 @@ class NN(object):
                 #compute forward pass
                 xBatch=self.X[i:i+batchSize]
                 yBatch=self.Y[i:i+batchSize]
-                actualOut=self.Evaluate(xBatch)
+                actualOut=self.Evaluate(xBatch,batchNorm,batchMode=BatchNormMode.TRAIN)
+                #print(actualOut)
                 if(self.lastLayerAF==ActivationType.SOFTMAX):
-                    loss+=-(yBatch*np.log(actualOut)).sum()
+                    loss+=-(yBatch*np.log(actualOut+0.001)).sum()
                 else:
                     loss+=((actualOut-yBatch)*(actualOut-yBatch)).sum()/2
                 #backpropagation: updating weigths and biases
@@ -49,47 +60,49 @@ class NN(object):
                         else:
                             self.listOfLayers[levelNum].delta=(actualOut - yBatch)*self.listOfLayers[levelNum].gradAF
                     else:#regular layer
-                        #print(self.listOfLayers[levelNum+1].delta.shape)
-                        #print(self.listOfLayers[levelNum+1].W.shape)
-                        self.listOfLayers[levelNum].delta=self.listOfLayers[levelNum+1].delta@(self.listOfLayers[levelNum+1].W)*self.listOfLayers[levelNum].gradAF                    
+                        self.listOfLayers[levelNum].delta=self.listOfLayers[levelNum+1].delta@(self.listOfLayers[levelNum+1].W)*self.listOfLayers[levelNum].gradAF 
+                        if(batchNorm==True):
+                            self.listOfLayers[levelNum].GradBeta=self.listOfLayers[levelNum].delta.sum(axis=0)
+                            self.listOfLayers[levelNum].GradGamma=(self.listOfLayers[levelNum].delta*self.listOfLayers[levelNum].sHat).sum(axis=0)
+                            self.listOfLayers[levelNum].deltaBn=(self.listOfLayers[levelNum].delta*self.listOfLayers[levelNum].gamma)/(batchSize*((self.listOfLayers[levelNum].sigma2+epsilon)**0.5))*(batchSize-1-(self.listOfLayers[levelNum].sHat)**2)
+                            #self.listOfLayers[levelNum].deltaBn=(self.listOfLayers[levelNum].delta * self.listOfLayers[levelNum].gamma)/(batchSize*np.sqrt(self.listOfLayers[levelNum].sigma2 +self.listOfLayers[levelNum].epsilon )) * (batchSize - 1 - (self.listOfLayers[levelNum].sHat * self.listOfLayers[levelNum].sHat))
                     if(levelNum>0):
                         prevOut=self.listOfLayers[levelNum-1].a
                     else:#level #zero
-                        prevOut=xBatch
-                    #print(self.listOfLayers[levelNum].gradB.shape)
-                    #print(self.listOfLayers[levelNum].delta.shape)
-                    self.listOfLayers[levelNum].gradB+=self.listOfLayers[levelNum].delta.sum(axis=0)
-                    self.listOfLayers[levelNum].gradW+=self.listOfLayers[levelNum].delta.T@prevOut 
-                    levelNum-=1                
-                if(gradientType==GradientType.STOCHASTIC):
-                    self.UpdateGradsWB(alpha,regLambda)
-                if(gradientType==GradientType.MINIBATCH):
-                    self.UpdateGradsWB(alpha,regLambda,batchSize)
-            if(gradientType==GradientType.BATCH):
-                self.UpdateGradsWB(alpha,regLambda,samplesNum)
+                        prevOut=xBatch 
+                    if(batchNorm==True and self.listOfLayers[levelNum].isLastLayer==False):
+                        self.listOfLayers[levelNum].gradB+=self.listOfLayers[levelNum].deltaBn.sum(axis=0)
+                        self.listOfLayers[levelNum].gradW+=self.listOfLayers[levelNum].deltaBn.T@prevOut
+                    else:
+                        self.listOfLayers[levelNum].gradB+=self.listOfLayers[levelNum].delta.sum(axis=0)
+                        self.listOfLayers[levelNum].gradW+=self.listOfLayers[levelNum].delta.T@prevOut                        
+                    levelNum-=1  
+                self.UpdateGradsWB(alpha,regLambda,batchSize,batchNorm)
             print("epoch = ", j, "loss = ", loss)
 
-    def UpdateGradsWB(self, alpha,regLambda,batchSize=1):
+    def UpdateGradsWB(self, alpha,regLambda,batchSize,batchNorm=False):
         for level in self.listOfLayers:
             level.W=level.W-alpha*(level.gradW/batchSize)-alpha*regLambda*level.W
             level.B=level.B-alpha*(level.gradB/batchSize)
-            level.clearGradWB()
+            if(batchNorm==True):
+                level.beta=level.beta-alpha*(level.gradBeta/batchSize)
+                level.gamma=level.gamma-alpha*(level.gradGamma/batchSize)
+            level.clearGrads()
 
-    def GetAccuracy(self, testX, testY):
+    def GetAccuracy(self, testX, testY, batchNorm=False):
         rightCount=0
         testsNum=testX.shape[0]
         for i in range(testsNum):
             yi=testY[i]
             #forward pass:
-            a2=self.Evaluate(testX[i])
+            a2=self.Evaluate(testX[i],batchNorm,batchMode=BatchNormMode.TEST)
             maxIdx=np.argmax(a2)
             if(yi[maxIdx]==1):
                 rightCount+=1
         accuracy=(rightCount/testsNum)*100
         return accuracy
 
-    def Classify(self):
-        x=0
+
 
 
    